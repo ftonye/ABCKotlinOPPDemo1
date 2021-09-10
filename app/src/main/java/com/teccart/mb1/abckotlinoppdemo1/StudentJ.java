862e1676fae24285189638c5b30a6d5db3520abc
fernand.tonye@gmail.com <fernand.tonye@gmail.com>
2021-09-10 11:30:35 -0400

    Ajout constructeur et constucteur secondaire

--- app/src/main/java/com/teccart/mb1/abckotlinoppdemo1/StudentJ.java
+++ app/src/main/java/com/teccart/mb1/abckotlinoppdemo1/StudentJ.java
@@ -5,7 +5,22 @@ public class StudentJ {
     public int age;
     public double height;
 
-    public StudentJ(String name, int age, double height) {
+    public StudentJ() {  //equ secondaire #1 kotlin
+        this.name = "Jhon Doe";
+        this.age = 18;
+        this.height = 1.60;
+    }
+    public StudentJ(String name) {//equ secondaire #2 kotlin
+        this.name = name;
+        this.age = 18;
+        this.height = 1.60;
+    }
+    public StudentJ(String name, int age) {//equ secondaire #3 kotlin
+        this.name = name;
+        this.age = age;
+        this.height = 1.60;
+    }
+    public StudentJ(String name, int age, double height) { //equ primaire kotlin
         this.name = name;
         this.age = age;
         this.height = height;
--- app/src/main/java/com/teccart/mb1/abckotlinoppdemo1/StudentK.kt
+++ app/src/main/java/com/teccart/mb1/abckotlinoppdemo1/StudentK.kt
@@ -1,3 +1,8 @@
 package com.teccart.mb1.abckotlinoppdemo1
 
-class StudentK(var name:String,var age:Int,var height:Double)
+class StudentK(var name:String,var age:Int,var height:Double){
+    constructor():this("Jhon Doe",18,1.60)//secondaire #1
+    constructor(name: String):this(name,18,1.60)//secondaire #2
+    constructor(name: String,age: Int):this(name,age,1.60)//equ secondaire #3 kotlin
+
+}
