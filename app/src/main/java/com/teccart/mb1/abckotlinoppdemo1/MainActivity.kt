5ac1f50e13f890abc4def8211a7aa1dfd390ce87
Fernand Bikatal Bi Tonye <fernand.tonye@gmail.com>
2021-09-17 08:58:41 -0400

    creation du UI et enregistrement aux click envents

--- app/src/main/java/com/teccart/mb1/abckotlinoppdemo1/MainActivity.kt
+++ app/src/main/java/com/teccart/mb1/abckotlinoppdemo1/MainActivity.kt
@@ -2,13 +2,33 @@ package com.teccart.mb1.abckotlinoppdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  pw:EditText
+    private lateinit var  btn1:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var stj1:StudentJ = StudentJ("toto",25,1.80)
-        var stk1:StudentK = StudentK("tatou",20,1.60)
+        //this.pw = (EditText)findViewById(R.id.txt1);    code java
+        // this.pw = findViewById(R.id.txt1) as EditText;  code kotlin legacy
+       this.pw = findViewById<EditText>(R.id.txt1)
+
+        this.btn1 = findViewById<Button>(R.id.btnExit)
+        this.btn1.setOnClickListener(View.OnClickListener
+        {
+            this.pw.setText("Hello ")
+        })
+
+    }
+
+    fun Login(view: View) {
+        Toast.makeText(applicationContext,"tutu",Toast.LENGTH_LONG).show()
     }
 }
